c93a9c16a341f990675ce8cf72180d7d89394f9b
Mabouka <rahrah@gmail.com>
2021-10-22 10:11:29 +0200

    Ajoutez des fichiers projet.

---
+++ RP Dé/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            const int NOMBRE_MIN = 1;
+            const int NOMBRE_MAX = 10;
+            int NOMBRE_MAGIQUE = rand.Next(NOMBRE_MIN, NOMBRE_MAX + 1);
+
+            int nombre = NOMBRE_MAGIQUE + 1;
+
+            int nbVies = 4;
+
+            while (nbVies > 0)
+            {
+                Console.WriteLine("choisi un nombre entre 1 et 10");
+                string Nombre_Joueur = Console.ReadLine();
+                int nj = int.Parse(Nombre_Joueur);
+                if (NOMBRE_MAGIQUE > nj)
+                {
+                    Console.WriteLine("c'est plus grand");
+                    nbVies--;
+                    if (nbVies == 0)
+                    {
+                        Console.WriteLine("omae wa mou shindeiru");
+                    }
+                }
+                else if (NOMBRE_MAGIQUE < nj)
+                {
+                    Console.WriteLine("c'est plus petit");
+                    nbVies--;
+                    if (nbVies == 0)
+                    {
+                        Console.WriteLine("omae wa mou shindeiru");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Bravo tu a trouvé");
+                    nbVies = 0;
+                }
+            }
+        }
+    }
+}
---
+++ tableaux/question math/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace question_math
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int NombreMaxQuestion = 5; //Nombre de question au max
+            int NombreDeReussite = 0; //Nombre de reussite
+            int NombreActuel = 1; //Nombre de question actuel
+
+            while (NombreActuel < NombreMaxQuestion + 1)
+            {
+                Console.WriteLine("Question n°" + NombreActuel + "/" + NombreMaxQuestion + "\n");
+                NombreActuel = NombreActuel + 1;
+                Random rand = new Random();
+                const int min = 1;
+                const int max = 2;
+                int cequiadedans = rand.Next(min, max + 1);
+                int n = cequiadedans + 1;
+                if (cequiadedans == 1)
+                {
+                    Random rand2 = new Random();
+
+                    const int min2 = -50;
+                    const int max2 = 50;
+                    int V1 = rand.Next(min2, max2 + 1);
+                    int n1 = V1 + 1;
+                    int V2 = rand.Next(min2, max2 + 1);
+                    int n2 = V2 + 1;
+
+                    Console.Write(V1 + " + " + V2 + " = ");
+
+                    string reponse = Console.ReadLine();
+
+                    int r = getNumber(reponse);
+
+                    if ((V1 + V2) == r)
+                    {
+                        Console.WriteLine("GG mec une autre question\n\n");
+                        NombreDeReussite = NombreDeReussite + 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("NOP pas la bonne une autre question\n\n");
+                    }
+                }
+                else
+                {
+                    Random rand2 = new Random();
+
+                    const int min2 = -50;
+                    const int max2 = 50;
+                    int V1 = rand.Next(min2, max2 + 1);
+                    int n1 = V1 + 1;
+                    int V2 = rand.Next(min2, max2 + 1);
+                    int n2 = V2 + 1;
+                    Console.Write(V1 + " x " + V2 + " = ");
+                    string reponse = Console.ReadLine();
+
+                    int r = getNumber(reponse);
+
+                    if ((V1 * V2) == r)
+                    {
+                        Console.WriteLine("GG mec une autre question\n\n");
+                        NombreDeReussite = NombreDeReussite + 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("NOP pas la bonne une autre question\n\n");
+                    }
+                }
+
+            }
+
+            Console.WriteLine("Réponse juste : " + NombreDeReussite + "/" + NombreMaxQuestion);
+
+            if (NombreDeReussite == 2 || NombreDeReussite == 3)
+                Console.WriteLine("fait mieux mec");
+            else if (NombreDeReussite == 0)
+                Console.WriteLine("Mec va si recommence parce que le je suis pas fier de toi !!!!!!!!!!");
+            else if (NombreDeReussite == 5)
+                Console.WriteLine("bien jouer tu est pas débile tu veux une médaille ?");
+            else
+                Console.WriteLine("ok ça passe");
+
+        }
+
+        private static int getNumber(string entry)
+        {
+            bool state = int.TryParse(entry, out int r);
+
+            while (state == false)
+            {
+                Console.Write("Mec tu force pourquoi tu prend pas un nombre entier maintenant répond bien : ");
+                entry = Console.ReadLine();
+                state = int.TryParse(entry, out r);
+            }
+
+            return r;
+        }
+
+    }
+}
